72c43f57b59c94a8b5374ed2e53d246e0479c04e
Binbin Ye <binbin.ye@mujin.co.jp>
2024-09-20 12:46:08 +0900

    update version

--- gitlab-booster.js
+++ gitlab-booster.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         gitlab booster
 // @namespace    http://tampermonkey.net/
-// @version      2024-09-20
+// @version      1.0.0
 // @description  Boost productivity for code reviewers on gitlab
 // @author       braineo
 // @match        https://gitlab.com/*
